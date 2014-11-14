bf072fa9add5085fde45d6c49f488009358174b8
Henry Tam <IamHenry@Henrys-MacBook-Air.local>
2014-11-13 17:04:26 -0800

    js start

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-
+  $(".player-reroll").on("click", function(e){
+    e.preventDefault();
+    var response = $.ajax({
+      url: "/add_todo",
+      type: "get"
+    })
+    ajax.done(function(data){
+      alert("good job");
+    })
+    ajax.fail(function(data){
+      alert("fuuuuuuuuu");
+    })
 });
